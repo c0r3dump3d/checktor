c51987484e53a4b6b8ca3af6e22170372ee55ce4
coredump <coredump@autistici.org>
2016-05-11 14:59:10 +0200

    New and very basic script

--- checktor.py
+++ checktor.py
@@ -1,10 +1,27 @@
-def checkTor(ip):
-        ip_exit_tor_relay=[]
-        tor = urllib.urlopen('http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv')
-        for ip_tor in tor.readlines():
-                ip_tor = ip_tor.replace("\n","")
-                ip_exit_tor_relay.append(ip_tor)
-        if ip in ip_exit_tor_relay:
-                print " it's a TOR exit node."
-        else:
-                print " it's NOT a TOR exit node."
+
+#!/usr/bin/python
+
+import urllib
+from json import load
+from urllib2 import urlopen
+
+ip = load(urlopen('https://api.ipify.org/?format=json'))['ip']
+tor = urllib.urlopen('https://check.torproject.org/exit-addresses')
+
+for ip_tor in tor.readlines():
+       ip_tor = ip_tor.replace("\n","")
+       if "ExitAddress" in ip_tor:
+           ip_tor = ip_tor.split(" ")[1]
+           if ip == ip_tor:
+               success = 1
+               break
+           else:
+               success = 0
+
+if success == 1:
+
+        print "Your public Ip: " + ip + " It's a TOR exit node"
+
+else:
+
+        print "Your public Ip: " + ip + " It's NOT TOR exit node"
